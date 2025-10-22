4c733a2b2dfe1fb5cdf9e920b0072e49f653b71d
xanorbkid <corps.bronax@gmail.com>
2025-10-22 17:31:14 +0000

    Project setup

---
+++ HRIS/settings.py
@@ -0,0 +1,145 @@
+
+
+from pathlib import Path
+import os
+from decouple import config
+import dj_database_url
+import cloudinary
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = config('SECRET_KEY', default=None)
+if not SECRET_KEY:
+    raise Exception("SECRET_KEY is missing in .env")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = config('DEBUG', default=False, cast=bool)
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://127.0.0.1:8000/' 
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+
+# # Trusted Origin for CSRF_Token
+CSRF_TRUSTED_ORIGINS = [
+     'http://127.0.0.1:8000'
+]
+
+
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'jazzmin',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'Auth',
+    'cloudinary', 'cloudinary_storage',
+    'tinymce',
+    'Employee',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", 
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'HRIS.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'HRIS.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        # ssl_require=True,
+        default=config('DATABASE_URL')
+    )
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Static files for Railway
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
---
+++ HRIS/urls.py
@@ -0,0 +1,28 @@
+
+from django.contrib import admin
+
+"""
+URL Configuration for HRIS Project
+This module defines the main URL patterns for the Human Resource Information System (HRIS).
+It includes routes for admin interface, authentication, and employee management.
+URL Patterns:
+    - admin/: Django admin interface
+    - auth/: Authentication related URLs
+    - employee/: Employee management related URLs
+The static() helper is used to serve media files during development.
+Note:
+    Make sure DEBUG=True only in development environment when using static()
+"""
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('auth/', include('Auth.urls')),
+    path('employee/', include('Employee.urls')),
+
+
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
